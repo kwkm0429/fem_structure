158102e09ff01528ad3982e72a458343caf8b552
Kosuke Kawakami <kwkm0429@gmail.com>
2022-03-05 14:43:24 +0900

    update

--- include/time_measure.h
+++ include/time_measure.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include <chrono>
+#include <fstream>
+#include <string>
+#include <iomanip>
 
 #define MEASURE
 
 void initializeTime();
 double elapsedTime();
+void writeTime(std::string file_name, std::string func, double t_start, double t_end);
--- src/time_measure.cpp
+++ src/time_measure.cpp
@@ -1,4 +1,4 @@
-#include <chrono>
+#include "time_measure.h"
 
 #define MEASURE
 
@@ -11,4 +11,11 @@ void initializeTime(){
 double elapsedTime(){
 	std::chrono::system_clock::time_point cur_time = std::chrono::system_clock::now();
 	return static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(cur_time - init_time).count() / 1000.0);
+}
+
+void writeTime(std::string file_name, std::string func, double t_start, double t_end){
+	std::ofstream ofs(file_name, std::ios::app);
+	ofs<<std::left<<std::setw(30)<<func<<" : ";
+	ofs<<(t_end - t_start)/1000<<" [s]"<<std::endl;
+	ofs.close();
 }
